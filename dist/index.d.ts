97ee28843cbeeaf5102fb1e50d21265dc7231a16
desain <8593577+desain@users.noreply.github.com>
2025-04-17 21:44:41 +0000

    Apply automatic changes

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -7,4 +7,5 @@ export * from "./utils/GridParsed";
 export * from "./utils/itemUtils";
 export * from "./utils/jsUtils";
 export * from "./utils/obrTypeUtils";
+export declare function complain(message: string): void;
 //# sourceMappingURL=index.d.ts.map
