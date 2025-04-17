42629fd68334689730bff07b44788fdab8a386ac
desain <8593577+desain@users.noreply.github.com>
2025-04-17 21:44:41 +0000

    Apply automatic changes

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,4 @@
+import OBR from "@owlbear-rodeo/sdk";
 export { Control } from "./ui/Control";
 export { PluginGate } from "./ui/PluginGate";
 export { PluginThemeProvider } from "./ui/PluginThemeProvider";
@@ -7,4 +8,8 @@ export * from "./utils/GridParsed";
 export * from "./utils/itemUtils";
 export * from "./utils/jsUtils";
 export * from "./utils/obrTypeUtils";
+export function complain(message) {
+    console.error(message);
+    void OBR.notification.show(message, "ERROR");
+}
 //# sourceMappingURL=index.js.map
