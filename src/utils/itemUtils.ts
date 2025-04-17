d31c018692279e409cfab6b1ba765a973fa059bc
desain <nicholassdesai@gmail.com>
2025-04-17 12:44:46 -0500

    Add getName

--- src/utils/itemUtils.ts
+++ src/utils/itemUtils.ts
@@ -1,4 +1,11 @@
-import { Item } from "@owlbear-rodeo/sdk";
+import { Image, isImage, Item } from "@owlbear-rodeo/sdk";
+
+export function getName(item: Image | Item): string {
+    if (isImage(item) && item.text.plainText) {
+        return item.text.plainText;
+    }
+    return item.name;
+}
 
 export function getId(item: Item): string {
     return item.id;
