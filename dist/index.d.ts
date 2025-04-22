75bd60e996b6edb92fb9d7323bf5d94eb94c311b
desain <8593577+desain@users.noreply.github.com>
2025-04-22 18:50:49 +0000

    Apply automatic changes

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -3,6 +3,7 @@ export { PluginGate } from "./ui/PluginGate";
 export { PluginThemeProvider } from "./ui/PluginThemeProvider";
 export { useActionResizer } from "./ui/useActionResizer";
 export { useUndoRedoHandler } from "./ui/useUndoRedoHandler";
+export { useRehydrate } from "./ui/useRehydrate";
 export * from "./utils/GridParsed";
 export * from "./utils/itemUtils";
 export * from "./utils/jsUtils";
