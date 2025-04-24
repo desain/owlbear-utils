a88cf506a6323a69cdadc974e972801b11bbc02d
desain <nicholassdesai@gmail.com>
2025-04-23 22:59:49 -0500

    Export math

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ export * from "./utils/GridParsed";
 export * from "./utils/itemUtils";
 export * from "./utils/jsUtils";
 export * from "./utils/obrTypeUtils";
+export * from "./utils/mathUtils";
 
 export function complain(message: string) {
   console.error(message);
