f976764d8cbb1ef1afc54b2e889938a2de9a4113
desain <8593577+desain@users.noreply.github.com>
2025-04-29 23:13:03 +0000

    Apply automatic changes

--- dist/utils/obrTypeUtils.d.ts
+++ dist/utils/obrTypeUtils.d.ts
@@ -1,4 +1,4 @@
-import { BlendMode, CurveStyle, Layer, ShapeStyle, Vector2 } from "@owlbear-rodeo/sdk";
+import OBR, { BlendMode, CurveStyle, Layer, ShapeStyle, Vector2 } from "@owlbear-rodeo/sdk";
 import { Vector3 } from "@owlbear-rodeo/sdk/lib/types/Vector3";
 export declare const BLEND_MODES: BlendMode[];
 export declare function isBlendMode(mode: unknown): mode is BlendMode;
@@ -8,4 +8,5 @@ export declare function isVector2(v: unknown): v is Vector2;
 export declare function isVector3(v: unknown): v is Vector3;
 export declare function isShapeStyle(style: unknown): style is ShapeStyle;
 export declare function isCurveStyle(style: unknown): style is CurveStyle;
+export type Role = Awaited<ReturnType<typeof OBR.player.getRole>>;
 //# sourceMappingURL=obrTypeUtils.d.ts.map
