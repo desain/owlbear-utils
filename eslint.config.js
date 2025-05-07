c26b2f44315f275c9fb3592230e6659791ff84c5
Nicholas Desai <nicholassdesai@gmail.com>
2025-05-07 02:15:04 -0500

    Add import extension lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,14 @@
 "use strict";
 import customConfig from "eslint-config";
+import eslintPluginImport from "eslint-plugin-import";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config({
     extends: [...customConfig],
+    plugins: {
+        import: eslintPluginImport,
+    },
+    rules: {
+        "import/extensions": ["error", "ignorePackages"],
+    },
 });
