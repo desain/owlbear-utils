0f64834cf0ef472644612cd641e6b3f202756430
Nick <nicholassdesai@gmail.com>
2025-05-27 12:41:16 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,36 @@
 import OBR from "@owlbear-rodeo/sdk";
 
+// Abstract Interaction
 export {
     createLocalInteraction,
     wrapRealInteraction,
 } from "./AbstractInteraction.js";
 export type { AbstractInteraction } from "./AbstractInteraction.js";
+
+// ObrAsyncFunction
+export {
+    compileObrAsyncFunction,
+    executeObrAsyncFunction,
+};
+export type { ObrAsyncFunction } from "./ObrAsyncFunction.js";
+
+// Number utils
 export { cells, floorCells, roundCells, pixels, units, unitsToCells, pixelsToCells, cellsToPixels } from "./utils/numberUtils.js";
 export type { Cells, CellsWhole, Pixels, Units } from "./utils/numberUtils.js";
+
+// Color utils
 export type { RgbColor, HexColor } from "./utils/colorUtils.js";
 export { isHexColor, assumeHexColor, hexToRgb, rgbToHex, WHITE_HEX, PINK_RGB, WHITE_RGB, RED_RGB, YELLOW_RGB } from "./utils/colorUtils.js"
+
+// UI
 export { Control } from "./ui/Control.js";
 export { PluginGate } from "./ui/PluginGate.js";
 export { PluginThemeProvider } from "./ui/PluginThemeProvider.js";
 export { useActionResizer } from "./ui/useActionResizer.js";
 export { startRehydrating, useRehydrate } from "./ui/useRehydrate.js";
 export { useUndoRedoHandler } from "./ui/useUndoRedoHandler.js";
+
+// Misc utils
 export * from "./utils/GridParsed.js";
 export * from "./utils/itemUtils.js";
 export * from "./utils/jsUtils.js";
