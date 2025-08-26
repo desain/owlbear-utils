14e935d2f74a3ecd788cf8642c195276d070415f
desain <8593577+desain@users.noreply.github.com>
2025-08-26 02:16:46 +0000

    Apply automatic changes

--- dist/utils/jsUtils.d.ts
+++ dist/utils/jsUtils.d.ts
@@ -1,6 +1,6 @@
 export declare function isObject(object: unknown): object is object;
 export declare function isDeepEqual<T extends object>(object1: T | null | undefined, object2: T | null | undefined): boolean;
-export declare function groupBy<T, K extends string>(ts: T[], key: (t: T) => K): Record<K, T[]>;
+export declare function groupBy<T, K extends string | number | symbol>(ts: T[], key: (t: T) => K): Record<K, T[]>;
 export declare function deferCallAll(...functions: VoidFunction[]): VoidFunction;
 export declare function getOrInsert<K, V>(m: Map<K, V>, key: K, insert: () => V): V;
 /**
