42c70c26f04357100491037809a3a72d011a73d0
desain <8593577+desain@users.noreply.github.com>
2025-09-10 07:01:46 +0000

    Apply automatic changes

--- dist/index.js
+++ dist/index.js
@@ -5,7 +5,8 @@ export { createLocalInteraction, wrapRealInteraction, } from "./AbstractInteract
 export { compileObrFunction, executeObrFunction, compileObrAsyncFunction, executeObrAsyncFunction, } from "./ObrFunction.js";
 // Number utils
 export * from "./utils/numberUtils.js";
-export { isHexColor, assumeHexColor, hexToRgb, rgbToHex, WHITE_HEX, PINK_RGB, WHITE_RGB, RED_RGB, YELLOW_RGB } from "./utils/colorUtils.js";
+// Color utils
+export * from "./utils/colorUtils.js";
 // UI
 export { Control } from "./ui/Control.js";
 export { ExtensionWrapper, getTheme } from "./ui/ExtensionWrapper.js";
