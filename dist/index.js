9131e35d0a6c5a6c60719c85ce382d0cf476a9e3
Nicholas Desai <nicholassdesai@gmail.com>
2025-11-06 16:09:20 -0600

    fix complain

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,9 @@
 import OBR from "@owlbear-rodeo/sdk";
+import { isOwlbearError } from "./utils/obrTypeUtils.js";
 // Abstract Interaction
 export { createLocalInteraction, wrapRealInteraction, } from "./AbstractInteraction.js";
 // ObrAsyncFunction
-export { compileObrFunction, executeObrFunction, compileObrAsyncFunction, executeObrAsyncFunction, } from "./ObrFunction.js";
+export { compileObrAsyncFunction, compileObrFunction, executeObrAsyncFunction, executeObrFunction, } from "./ObrFunction.js";
 // UI
 export { Control } from "./ui/Control.js";
 export { ExtensionWrapper, getTheme } from "./ui/ExtensionWrapper.js";
@@ -11,23 +12,30 @@ export * from "./ui/usePopoverResizer.js";
 export { startRehydrating, useRehydrate } from "./ui/useRehydrate.js";
 export { useUndoRedoHandler } from "./ui/useUndoRedoHandler.js";
 // Misc utils
+export * from "./defaults.js";
+export * from "./ItemMap.js";
+export * from "./utils/bezierUtils.js";
+export * from "./utils/colorUtils.js";
+export * from "./utils/debug.js";
+export * from "./utils/getWorldPoints.js";
 export * from "./utils/GridParsed.js";
+export * from "./utils/gridUtils.js";
 export * from "./utils/itemUtils.js";
 export * from "./utils/jsUtils.js";
 export * from "./utils/mathUtils.js";
+export * from "./utils/numberUtils.js";
 export * from "./utils/obrTypeUtils.js";
 export * from "./utils/sceneUtils.js";
-export * from "./utils/bezierUtils.js";
-export * from "./utils/gridUtils.js";
-export * from "./utils/colorUtils.js";
-export * from "./utils/numberUtils.js";
-export * from "./utils/debug.js";
-export * from "./utils/getWorldPoints.js";
-export * from "./ItemMap.js";
-export * from "./watcher/Watcher.js";
 export * from "./watcher/Patcher.js";
-export * from "./defaults.js";
-export function complain(message) {
+export * from "./watcher/Watcher.js";
+export function complain(e) {
+    const message = typeof e === "string"
+        ? e
+        : e instanceof Error
+            ? e.message
+            : isOwlbearError(e)
+                ? e.error.message
+                : String(e);
     console.error(message);
     void OBR.notification.show(message, "ERROR");
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import OBR from "@owlbear-rodeo/sdk";
+import { isOwlbearError } from "./utils/obrTypeUtils.js";
 
 // Abstract Interaction
 export {
@@ -9,12 +10,12 @@ export type { AbstractInteraction } from "./AbstractInteraction.js";
 
 // ObrAsyncFunction
 export {
-    compileObrFunction,
-    executeObrFunction,
     compileObrAsyncFunction,
+    compileObrFunction,
     executeObrAsyncFunction,
+    executeObrFunction,
 } from "./ObrFunction.js";
-export type { ObrFunction, ObrAsyncFunction } from "./ObrFunction.js";
+export type { ObrAsyncFunction, ObrFunction } from "./ObrFunction.js";
 
 // UI
 export { Control } from "./ui/Control.js";
@@ -26,24 +27,32 @@ export { startRehydrating, useRehydrate } from "./ui/useRehydrate.js";
 export { useUndoRedoHandler } from "./ui/useUndoRedoHandler.js";
 
 // Misc utils
+export * from "./defaults.js";
+export * from "./ItemMap.js";
+export * from "./utils/bezierUtils.js";
+export * from "./utils/colorUtils.js";
+export * from "./utils/debug.js";
+export * from "./utils/getWorldPoints.js";
 export * from "./utils/GridParsed.js";
+export * from "./utils/gridUtils.js";
 export * from "./utils/itemUtils.js";
 export * from "./utils/jsUtils.js";
 export * from "./utils/mathUtils.js";
+export * from "./utils/numberUtils.js";
 export * from "./utils/obrTypeUtils.js";
 export * from "./utils/sceneUtils.js";
-export * from "./utils/bezierUtils.js"
-export * from "./utils/gridUtils.js";
-export * from "./utils/colorUtils.js"
-export * from "./utils/numberUtils.js";
-export * from "./utils/debug.js";
-export * from "./utils/getWorldPoints.js";
-export * from "./ItemMap.js";
-export * from "./watcher/Watcher.js";
 export * from "./watcher/Patcher.js";
-export * from "./defaults.js";
+export * from "./watcher/Watcher.js";
 
-export function complain(message: string) {
+export function complain(e: unknown) {
+    const message =
+        typeof e === "string"
+            ? e
+            : e instanceof Error
+            ? e.message
+            : isOwlbearError(e)
+            ? e.error.message
+            : String(e);
     console.error(message);
     void OBR.notification.show(message, "ERROR");
 }
