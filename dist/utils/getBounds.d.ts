91f53d3a262866c1f7c506485425b24fa63dac66
desain <8593577+desain@users.noreply.github.com>
2025-11-25 18:48:28 +0000

    Apply automatic changes

--- dist/utils/getBounds.d.ts
+++ dist/utils/getBounds.d.ts
@@ -1,7 +1,6 @@
 import { type BoundingBox, type Curve, type Image, type Item, type Line, type Path, type Shape, type Wall } from "@owlbear-rodeo/sdk";
 import type { GridParams } from "./GridParsed.js";
-type BoundableItem = Image | Shape | Line | Curve | Path | Wall;
+export type BoundableItem = Image | Shape | Line | Curve | Path | Wall;
 export declare function isBoundableItem(item: Item): item is BoundableItem;
 export declare function getBounds(item: BoundableItem, grid: GridParams): BoundingBox;
-export {};
 //# sourceMappingURL=getBounds.d.ts.map
